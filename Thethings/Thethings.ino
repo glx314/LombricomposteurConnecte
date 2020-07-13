123e5697474a91a4cf84eb352c97e213ccd54e28
Guitou <guillaume.branche@gmail.com>
2020-07-13 16:36:50 +0200

    Ajout d'un Lora encoder à tester au bureau

--- Thethings/Thethings.ino
+++ Thethings/Thethings.ino
@@ -1,24 +1,37 @@
 #include <TheThingsNetwork.h>
-#include "DHT.h"
+#include <LoraEncoder.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS 2 
+/********************************************************************/
+// Setup a oneWire instance to communicate with any OneWire devices  
+// (not just Maxim/Dallas temperature ICs) 
+OneWire oneWire(ONE_WIRE_BUS); 
+/********************************************************************/
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
 // Set your AppEUI and AppKey
 const char *appEui = "0000000000000000";
 const char *appKey = "97a685c64a870a4f72db1015b81dcaf3";
-#define DHTTYPE DHT22
-#define DHTPIN A4
+const int humiditySensorPin=A0; //réglages humidité
+
 #define loraSerial Serial1
 #define debugSerial Serial
 
+int humidity;
+int pHumidity;
+float DS18B20_temperature;
 // Replace REPLACE_ME with TTN_FP_EU868 or TTN_FP_US915
 #define freqPlan TTN_FP_EU868
 
 TheThingsNetwork ttn(loraSerial, debugSerial, freqPlan);
-DHT dht(DHTPIN,DHTTYPE);
+
  
 void setup()
 {
   loraSerial.begin(57600);
   debugSerial.begin(9600);
-  dht.begin();
   // Wait a maximum of 10s for Serial Monitor
   while (!debugSerial && millis() < 10000)
     ;
@@ -33,20 +46,19 @@ void setup()
 void loop()
 {
   debugSerial.println("-- LOOP");
-  uint32_t h = dht.readHumidity();//on lit l'hygrometrie
-  uint32_t t = dht.readTemperature();
-  if (isnan(h) || isnan(t))
- {
-   Serial.println("Failed to read from DHT sensor!");
-   return;
- }
+  sensors.requestTemperatures();
+  
+  DS18B20_temperature = sensors.getTempCByIndex(0);
 
-    byte payload[4];
-    payload[0]=highByte(h);
-    payload[1]= lowByte(h);
-    payload[2] = highByte(t);
-    payload[3] = highByte(t);
-  ttn.sendBytes(payload,sizeof(payload));
+    humidity=analogRead(humiditySensorPin);
+    pHumidity=map(humidity,0,605,0,100);//on lit l'hygrometrie
+
+    byte buffer[4];
+    LoraEncoder encoder(buffer);
+    encoder.writeTemperature(DS18B20_temperature);
+    encoder.writeHumidity(pHumidity);
+    
+  ttn.sendBytes(buffer,sizeof(buffer));
 
   delay(10000);
 }
