a06d211f7a1bb033feb6659eb4674f40995a0328
GuitouBDA <guillaume.branche@gmail.com>
2020-07-20 10:57:37 +0200

    indenté et encodé

--- AvecBouttons/AvecBouttons.ino
+++ AvecBouttons/AvecBouttons.ino
@@ -26,6 +26,8 @@ DallasTemperature sensors(&oneWire);
 
 LiquidCrystal screen(12,11,A2,A3,A4,A5);
 
+const int TempsReleve = 10000;
+
 float DS18B20_temperature; //réglages température
 
 const int humiditySensorPin=A0; //réglages humidité
@@ -65,6 +67,254 @@ float Offset = 0;
 float VA;
 float V0;
 
+void setup(){
+  
+  loraSerial.begin(57600);
+  debugSerial.begin(9600);
+  screen.begin(16,2);
+  screen.clear();
+  sensors.begin();
+  
+  pinMode(maintenanceButtonPin, INPUT_PULLUP);
+  pinMode(testpHButtonPin,INPUT_PULLUP);
+  pinMode(calibNeutreButtonPin,INPUT_PULLUP);
+  pinMode(calibAcidButtonPin,INPUT_PULLUP);
+  pinMode(maintenanceLEDPin,OUTPUT);
+  pinMode(testpHLEDPin,OUTPUT);
+  pinMode(calibNeutreLEDPin,OUTPUT);
+  pinMode(calibAcidLEDPin,OUTPUT);
+  pinMode(ONE_WIRE_BUS,INPUT);
+  pinMode(humiditySensorPin,INPUT);
+  
+   // Wait a maximum of 10s for Serial Monitor
+  while (!debugSerial && millis() < 10000);
+
+  debugSerial.println("-- STATUS");
+  ttn.showStatus();
+  
+  debugSerial.println("-- JOIN");
+  ttn.join(appEui, appKey);
+  
+}
+
+void loop(){
+  
+  digitalWrite(maintenanceLEDPin,LOW); //Activation des leds
+  digitalWrite(testpHLEDPin,LOW);
+  digitalWrite(calibNeutreLEDPin,LOW);
+  digitalWrite(calibAcidLEDPin,LOW);
+
+  maintenance = not(digitalRead(maintenanceButtonPin));
+  testpH=not(digitalRead(testpHButtonPin));
+  calibNeutre=not(digitalRead(calibNeutreButtonPin));
+  calibAcid=not(digitalRead(calibAcidButtonPin));
+  sensors.requestTemperatures();
+  DS18B20_temperature = sensors.getTempCByIndex(0);
+  humidity=analogRead(humiditySensorPin);
+  
+  if(compteur==TempsReleve){
+    debugSerial.println("-- LOOP");
+     //Mesure température
+    Serial.println(DS18B20_temperature);
+  
+    
+    pHumidity=map(humidity,0,605,0,100);
+    Serial.println(pHumidity); // ATTENTION : TESTER LES VALEURS POUR MAPPER EN RECEVANT LE CAPTEUR
+
+    screen.clear();
+    screen.setCursor(0,0);
+    screen.print("Temp : ");
+    screen.print(DS18B20_temperature);
+    screen.print(" ");
+    screen.print((char)223);
+    screen.println("C     ");
+    screen.setCursor(0,1);
+    screen.print("Humidite : ");
+    screen.print(pHumidity);
+    screen.print(" %");
+
+    byte TPayload[2];
+    int sizeT=sizeof(TPayload);
+    byte HPayload[1];
+    int sizeH=sizeof(HPayload);
+    int T=round(DS18B20_temperature*10.0);
+    TPayload[0]=highByte(T);
+    TPayload[1]=lowByte(T);
+    byte H;
+    H=(int)pHumidity;
+    HPayload[0]=H;
+
+    byte payload[sizeT + sizeH];
+    memcpy(payload, TPayload, sizeT);
+    memcpy(payload + sizeT, HPayload, sizeH);
+    
+    
+    ttn.sendBytes(payload,sizeof(payload));
+    for(int i=0;i<sizeof(payload);i++){
+      Serial.print(payload[i]);
+      Serial.print(";");
+    }
+    Serial.println("");
+
+  }
+  
+  if (maintenance){fMaintenance();}
+  
+  if (testpH){fTestpH();}
+  
+  if (calibNeutre){fCalibNeutre();}
+  
+  if (calibAcid){ fCalibAcide();}
+   
+  compteur+=5000;
+  if(compteur>TempsReleve){
+      compteur=0;
+  }
+  
+  delay(5000); 
+  
+}
+
+
+void fMaintenance(){
+       //Maintenance : on ne prend plus de mesures
+    digitalWrite(maintenanceLEDPin,HIGH);
+    int minute=TempsMaintenance;
+    int seconde=10;
+    while(seconde != 0 || minute != 0){
+    screen.clear();
+    screen.setCursor(0,0);
+    screen.print("Maintenance");
+    screen.setCursor(0,1);
+    screen.print("Patientez  ");  
+    screen.print(minute);
+    screen.print(":");
+      
+    if(seconde<10){screen.print("0");}
+    
+    screen.print(seconde);
+    
+    if(seconde == 0 && minute!=0){
+        minute--;
+        seconde=60;
+      }
+
+    for(int i=0;i<10;i++){
+        delay(100);
+        maintenance = not(digitalRead(maintenanceButtonPin));
+        if(maintenance){
+          minute=TempsMaintenance;
+          seconde=60;
+          }
+        }
+      
+    seconde--;
+      }
+      
+   }
+
+void fTestpH(){//Mesure du pH 
+  
+    digitalWrite(testpHLEDPin,HIGH);
+    int compteurPH=0;
+    while(compteurPH<tempsTestPH){
+      static unsigned long samplingTime = millis();
+      static unsigned long printTime = millis();
+      static float pHValue,voltage;
+  
+      if(millis()-samplingTime > samplingInterval){
+          pHArray[pHArrayIndex++]=analogRead(pHSensorPin);
+          if(pHArrayIndex==ArrayLenth)pHArrayIndex=0;
+          voltage = avergearray(pHArray, ArrayLenth)*5.0/1024;
+          pHValue = pente*voltage+Offset;
+          samplingTime=millis();
+              }
+      if(millis() - printTime > printInterval){   //Every 800 milliseconds, print a numerical, convert the state of the LED indicator
+  
+          screen.clear();
+          screen.setCursor(0,0);
+          screen.print("pH = ");
+          screen.print(pHValue);
+          printTime=millis();
+              }
+
+      compteurPH+=100;
+
+    
+      delay(100);
+       
+       }
+   }
+
+void fCalibNeutre(){
+     // Calibration sonde pH dans solution neutre
+    digitalWrite(calibNeutreLEDPin,HIGH);
+    int compteurPHN=0;
+    while(compteurPHN<tempsCalibPH){
+        static unsigned long samplingTime = millis();
+        static unsigned long printTime = millis();
+        static float pHValue,voltage;
+        if(millis()-samplingTime > samplingInterval){
+            pHArray[pHArrayIndex++]=analogRead(pHSensorPin);
+            if(pHArrayIndex==ArrayLenth)pHArrayIndex=0;
+            V0 = avergearray(pHArray, ArrayLenth)*5.0/1024;
+            voltage=V0;
+            pHValue = pente*voltage + Offset;
+            samplingTime=millis();
+            }
+            
+        if(millis() - printTime > printInterval){   //Every 800 milliseconds, print a numerical, convert the state of the LED indicator
+  
+            screen.clear();
+            screen.setCursor(0,0);
+            screen.print("pH = ");
+            screen.print(pHValue);
+            printTime=millis();
+             }
+
+        compteurPHN+=100; 
+        delay(100);
+        }
+
+  pente=3/(V0-VA);
+  Offset = 4-(3*VA)/(V0-VA);
+    
+  }
+
+ void fCalibAcide(){
+    // Calibration sonde pH dans solution acide
+    digitalWrite(calibAcidLEDPin,HIGH);
+    int compteurPHA=0;
+    while(compteurPHA<tempsCalibPH){
+        static unsigned long samplingTime = millis();
+        static unsigned long printTime = millis();
+        static float pHValue,voltage;
+  
+        if(millis()-samplingTime > samplingInterval){
+            pHArray[pHArrayIndex++]=analogRead(pHSensorPin);
+            if(pHArrayIndex==ArrayLenth)pHArrayIndex=0;
+            VA = avergearray(pHArray, ArrayLenth)*5.0/1024;
+            voltage=VA;
+            pHValue = pente*voltage+Offset;
+            samplingTime=millis();
+            }
+            
+        if(millis() - printTime > printInterval){   //Every 800 milliseconds, print a numerical, convert the state of the LED indicator
+  
+            screen.clear();
+            screen.setCursor(0,0);
+            screen.print("pH = ");
+            screen.print(pHValue);
+            printTime=millis();
+             }
+
+        compteurPHA+=100; 
+        delay(100);
+           }
+    
+    pente=3/(V0-VA);
+    Offset = 4 -(3*VA)/(V0-VA) ;
+    }  
 
 double avergearray(int* arr, int number){
   int i;
@@ -105,241 +355,4 @@ double avergearray(int* arr, int number){
     avg = (double)amount/(number-2);
   }//if
   return avg;
-    
-    
   }
-
-void setup()
-{
-  loraSerial.begin(57600);
-  debugSerial.begin(9600);
-  screen.begin(16,2);
-  screen.clear();
-  sensors.begin();
-  pinMode(maintenanceButtonPin, INPUT_PULLUP);
-  pinMode(testpHButtonPin,INPUT_PULLUP);
-  pinMode(calibNeutreButtonPin,INPUT_PULLUP);
-  pinMode(calibAcidButtonPin,INPUT_PULLUP);
-  pinMode(maintenanceLEDPin,OUTPUT);
-  pinMode(testpHLEDPin,OUTPUT);
-  pinMode(calibNeutreLEDPin,OUTPUT);
-  pinMode(calibAcidLEDPin,OUTPUT);
-  pinMode(ONE_WIRE_BUS,INPUT);
-  pinMode(humiditySensorPin,INPUT);
-   // Wait a maximum of 10s for Serial Monitor
-  while (!debugSerial && millis() < 10000)
-    ;
-
-  debugSerial.println("-- STATUS");
-  ttn.showStatus();
-  
-  debugSerial.println("-- JOIN");
-  ttn.join(appEui, appKey);
-  
-}
-
-void loop()
-{
-  
-  
-  digitalWrite(maintenanceLEDPin,LOW); //Activation des leds
-  digitalWrite(testpHLEDPin,LOW);
-  digitalWrite(calibNeutreLEDPin,LOW);
-  digitalWrite(calibAcidLEDPin,LOW);
-
-  maintenance = not(digitalRead(maintenanceButtonPin));
-  testpH=not(digitalRead(testpHButtonPin));
-  calibNeutre=not(digitalRead(calibNeutreButtonPin));
-  calibAcid=not(digitalRead(calibAcidButtonPin));
-  
-  if(compteur==3000){
-    debugSerial.println("-- LOOP");
-    sensors.requestTemperatures();
-  DS18B20_temperature = sensors.getTempCByIndex(0); //Mesure température
-  Serial.println(DS18B20_temperature);
-  
-  humidity=analogRead(humiditySensorPin);
-  pHumidity=map(humidity,0,605,0,100);
-  Serial.println(pHumidity); // ATTENTION : TESTER LES VALEURS POUR MAPPER EN RECEVANT LE CAPTEUR
-
-  screen.clear();
-  screen.setCursor(0,0);
-  screen.print("Temp : ");
-  screen.print(DS18B20_temperature);
-  screen.print(" ");
-  screen.print((char)223);
-  screen.println("C     ");
-  screen.setCursor(0,1);
-  screen.print("Humidite : ");
-  screen.print(pHumidity);
-  screen.print(" %");
-
-   byte buffer[4];
-    
-    
-  ttn.sendBytes(buffer,sizeof(buffer));
-
-  }
-  
-  if (maintenance){fMaintenance();}
-  
-  if (testpH){fTestpH();}
-  
-  if (calibNeutre){fCalibNeutre();}
-  
-  if (calibAcid){ fCalibAcide();}
-   
-  compteur+=100;
-  if(compteur>3000){
-      compteur=0;
-  }
-  
-  delay(100); 
-  
-}
-
-
-void fMaintenance(){
-       //Maintenance : on ne prend plus de mesures
-    digitalWrite(maintenanceLEDPin,HIGH);
-    int minute=TempsMaintenance;
-    int seconde=10;
-    while(seconde != 0 || minute != 0){
-    screen.clear();
-    screen.setCursor(0,0);
-    screen.print("Maintenance");
-    screen.setCursor(0,1);
-    screen.print("Patientez  ");  
-    screen.print(minute);
-    screen.print(":");
-      if(seconde<10){screen.print("0");}
-    screen.print(seconde);
-    
-      if(seconde == 0 && minute!=0){
-        minute--;
-        seconde=60;
-      }
-
-      for(int i=0;i<10;i++){
-         delay(100);
-         maintenance = not(digitalRead(maintenanceButtonPin));
-         if(maintenance){
-          minute=TempsMaintenance;
-          seconde=60;
-      }
-        }
-      seconde--;
-
-      
-      }
-      
-    }
-
-void fTestpH(){//Mesure du pH 
-  
-    digitalWrite(testpHLEDPin,HIGH);
-    int compteurPH=0;
-    while(compteurPH<tempsTestPH){
-      
-    static unsigned long samplingTime = millis();
-    static unsigned long printTime = millis();
-    static float pHValue,voltage;
-  
-  if(millis()-samplingTime > samplingInterval)
-  {
-      pHArray[pHArrayIndex++]=analogRead(pHSensorPin);
-      if(pHArrayIndex==ArrayLenth)pHArrayIndex=0;
-      voltage = avergearray(pHArray, ArrayLenth)*5.0/1024;
-      pHValue = pente*voltage+Offset;
-      samplingTime=millis();
-  }
-  if(millis() - printTime > printInterval)   //Every 800 milliseconds, print a numerical, convert the state of the LED indicator
-  {
-    screen.clear();
-    screen.setCursor(0,0);
-    screen.print("pH = ");
-    screen.print(pHValue);
-    
-        
-        printTime=millis();
-  }
-
-    compteurPH+=100;
-
-    
-    delay(100);
-    }
-    
-  }
-
-void fCalibNeutre(){
-     // Calibration sonde pH dans solution neutre
-    digitalWrite(calibNeutreLEDPin,HIGH);
-    int compteurPHN=0;
-    while(compteurPHN<tempsCalibPH){
-    static unsigned long samplingTime = millis();
-    static unsigned long printTime = millis();
-    static float pHValue,voltage;
-  if(millis()-samplingTime > samplingInterval)
-  {
-      pHArray[pHArrayIndex++]=analogRead(pHSensorPin);
-      if(pHArrayIndex==ArrayLenth)pHArrayIndex=0;
-      V0 = avergearray(pHArray, ArrayLenth)*5.0/1024;
-      voltage=V0;
-      pHValue = pente*voltage + Offset;
-      samplingTime=millis();
-  }
-  if(millis() - printTime > printInterval)   //Every 800 milliseconds, print a numerical, convert the state of the LED indicator
-  {
-    screen.clear();
-    screen.setCursor(0,0);
-    screen.print("pH = ");
-    screen.print(pHValue);
-    printTime=millis();
-  }
-
-    compteurPHN+=100; 
-    delay(100);
-    }
-
-  pente=3/(V0-VA);
-  Offset = 4-(3*VA)/(V0-VA);
-    
-  }
-
- void fCalibAcide(){
-    // Calibration sonde pH dans solution acide
-    digitalWrite(calibAcidLEDPin,HIGH);
-    int compteurPHA=0;
-    while(compteurPHA<tempsCalibPH){
-    static unsigned long samplingTime = millis();
-  static unsigned long printTime = millis();
-  static float pHValue,voltage;
-  
-  if(millis()-samplingTime > samplingInterval)
-  {
-      pHArray[pHArrayIndex++]=analogRead(pHSensorPin);
-      if(pHArrayIndex==ArrayLenth)pHArrayIndex=0;
-      VA = avergearray(pHArray, ArrayLenth)*5.0/1024;
-      voltage=VA;
-      pHValue = pente*voltage+Offset;
-      samplingTime=millis();
-  }
-  if(millis() - printTime > printInterval)   //Every 800 milliseconds, print a numerical, convert the state of the LED indicator
-  {
-    screen.clear();
-    screen.setCursor(0,0);
-    screen.print("pH = ");
-    screen.print(pHValue);
-    printTime=millis();
-  }
-
-    compteurPHA+=100; 
-    delay(100);
-    }
-
-    pente=3/(V0-VA);
-    Offset = 4 -(3*VA)/(V0-VA) ;
-  
-    
-    }
